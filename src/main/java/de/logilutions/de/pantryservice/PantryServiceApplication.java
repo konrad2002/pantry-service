b6095d74e5e6e3490266593563f7a968229b2250
konrad2002 <konrad@schwimmteamerzgebirge.de>
2022-03-11 12:35:17 +0100

    init

---
+++ src/main/java/de/logilutions/de/pantryservice/PantryServiceApplication.java
@@ -0,0 +1,13 @@
+package de.logilutions.de.pantryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PantryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PantryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/logilutions/de/pantryservice/PantryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.logilutions.de.pantryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
